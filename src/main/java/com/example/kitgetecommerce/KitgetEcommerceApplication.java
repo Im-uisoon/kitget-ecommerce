ddb06b6a1bfe1ff3a98842c3641c0f42d9e590c4
Im-uisoon <dla5420@naver.com>
2026-05-27 08:16:40 +0900

    Initial commit

---
+++ src/main/java/com/example/kitgetecommerce/KitgetEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitgetecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitgetEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitgetEcommerceApplication.class, args);
+    }
+
+}
